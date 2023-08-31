1c5f84736b461504a2e8cbafea32435e2bef9bd1
Ahmed Maged <ahmedmaged20188@gmail.com>
2023-08-31 23:20:31 +0300

    Mobile edits

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-white  ">
       <link rel="icon" href="/logow.png" />
-      <div className="sm:flex h-full w-full pt-12 pr-24 justify-end bg-white">
+      <div className="sm:flex h-full w-full pt-12 sm:pr-24 justify-end bg-white relative">
         <Image
           src="/logo.png"
           height={300}
@@ -14,8 +14,8 @@ export default function Home() {
           className="m-auto sm:m-0"
         />
       </div>
-      <div className="justify-center h-full w-full pt-20">
-        <p className="text-center text:2xl sm:text-4xl font-Madani">
+      <div className="justify-center h-full w-full py-24">
+        <p className="text-center text:3xl sm:text-4xl font-Madani">
           <span className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white">
             ...
           </span>
@@ -23,7 +23,7 @@ export default function Home() {
         </p>
       </div>
       <div className="justify-bottom h-full w-full pt-36 sm:pt-16 bg-white">
-        <Image src={bg} height={100} width={2000} alt="bg" />
+        <Image src={bg} height={300} width={2000} alt="bg" />
       </div>
     </div>
   );
